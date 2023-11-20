33def288f7bf10b4f9fe14b2cc950fc0bd1851e4
Crossedfall <ping@crossedfall.io>
2023-11-19 21:07:16 -0600

    Bumps to 2.1

--- src/bapi/cfg.py
+++ src/bapi/cfg.py
@@ -4,4 +4,4 @@ API = yaml.load(open("bapi/config/api.yml"), Loader=yaml.SafeLoader)
 SERVERS = yaml.load(open("bapi/config/servers.yml"), Loader=yaml.SafeLoader)
 PRIVATE = yaml.load(open("bapi/config/private.yml"), Loader=yaml.SafeLoader)
 
-VERSION = "2.0"
+VERSION = "2.1"
